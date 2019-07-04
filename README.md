0dc3fd95b012b47977934b11163198731a8a182e
pyjiang <pyjiang@uwcchina.org>
2019-07-04 21:47:30 +0800

    Improved interface and functionalities

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ The Schulte Grid Game made with JavaScript!
 
 ### Version description
 
-This version includes the most minimal but essential functionalities, logic and styling.
+This version comes as a great imporvement of the last on styling and animation, also code refactor.
 
 ### How to run
 
@@ -11,8 +11,5 @@ Open the ```index.html``` file with your browser and start to play!
 
 ### Features to be included
 
-1. Activate the "New Game" button.
-2. Will return time used after the game.
 3. Add more difficulty levels.
-4. Include better styling.
 5. Refactor the code.
--- script.js
+++ script.js
@@ -1,19 +1,49 @@
 // Variables
-var sideLength = 5;
-var size = sideLength * sideLength;
-var blocksDone = 0;
-var isGameOver = false;
+var sideLength;
+var size;
+var blocksDone;
+var startTime;
+var endTime;
 
 init();
 
 // Functions
 // Initialize the program with a default side length of 5
 function init() {
-    // Generate a 5x5 Schulte Grid
+    reset(5);
+    // Add event listener for the reset button
+    document.querySelector("button").addEventListener("click", function() {
+        reset(sideLength);
+    });
+}
+
+function reset(num) {
+    sideLength = num;
+    size = num * num;
+    blocksDone = startTime = endTime = 0;
+    // Generate a Schulte Grid
     var tblStr = getTblStr(sideLength);
     // Put it into the page (remember that innerHTML is not a function)
-    document.querySelector("div.grid").innerHTML = tblStr;
+    document.querySelector("div#grid").innerHTML = tblStr;
     loadEventListeners();
+}
+
+function getTblStr(sideLength) {
+    // Generate a number serie according to side length
+    var numbers = getRandNumSerie(size);
+    // Turn that serie into an HTML formatted table
+    var tblStr = "<table cellspacing=\"0\">";
+    for (var i = 0; i < sideLength; i++) {
+        tblStr += "<tr>"
+        for (var j = 0; j < sideLength; j++) {
+            tblStr += "<td>"
+            tblStr += numbers[sideLength * i + j];
+            tblStr += "<//td>"
+        }
+        tblStr += "<//tr>"
+    }
+    tblStr += "<//table>";
+    return tblStr;
 }
 
 // Generate a random number serie according to the specified size
@@ -37,24 +67,6 @@ function getRandNumSerie(size) {
     return numSerie;
 }
 
-function getTblStr(sideLength) {
-    // Generate a number serie according to side length
-    var numbers = getRandNumSerie(size);
-    // Turn that serie into an HTML formatted table
-    var tblStr = "<table>";
-    for (var i = 0; i < sideLength; i++) {
-        tblStr += "<tr>"
-        for (var j = 0; j < sideLength; j++) {
-            tblStr += "<td>"
-            tblStr += numbers[sideLength * i + j];
-            tblStr += "<//td>"
-        }
-        tblStr += "<//tr>"
-    }
-    tblStr += "<//table>";
-    return tblStr;
-}
-
 // Add event listeners for every block
 function loadEventListeners() {
     var tds = document.querySelectorAll("td");
@@ -62,12 +74,21 @@ function loadEventListeners() {
         tds[i].addEventListener("click", function() {
             // If the player is clicking in the right order, text turns green
             if (this.textContent == blocksDone + 1) {
+                if (blocksDone === 0) {
+                    startTime = new Date();
+                }
                 this.classList.add("done");
                 blocksDone++;
-            }
-            if (blocksDone === size) {
-                isGameOver = true;
-                alert("You won!");
+                if (blocksDone === size) {
+                    endTime = new Date();
+                    var timeTaken = endTime - startTime;
+                    // document.querySelector("table").classList.add("vanish");
+                    document.querySelector("#grid").innerHTML =
+                        "<p id=\"message\">" + "You have finished in " +
+                        "<span id=\"highlight\">" +
+                        Math.round(timeTaken / 1000) +
+                        "</span>" + " seconds!" + "</p>";
+                }
             }
         });
     }
